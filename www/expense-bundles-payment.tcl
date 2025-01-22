6d9c4c370e3bc5120204d4209689d5a6148f910e
Frank Bergmann <frank.bergmann@project-open.com>
2025-01-22 16:50:07 +0100

    Added -report_name to im_report_write_http_headers

--- www/expense-bundles-payment.tcl
+++ www/expense-bundles-payment.tcl
@@ -283,7 +283,7 @@ set start_days {01 1 02 2 03 3 04 4 05 5 06 6 07 7 08 8 09 9 10 10 11 11 12 12 1
 #
 
 # Write out HTTP header, considering CSV/MS-Excel formatting
-im_report_write_http_headers -output_format $output_format
+im_report_write_http_headers -output_format $output_format -report_name "expense-bundles-payment"
 
 # Add the HTML select box to the head of the page
 switch $output_format {
